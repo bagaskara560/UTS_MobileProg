2399c753eada64b02c535aa2580afd3088d6a286
Bagaskara <djordi.535200059@stu.untar.ac.id>
2022-03-25 15:23:05 +0700

    commit

---
+++ lib/Controllers/task_controller.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_to_do_app/db/db_helper.dart';
+import 'package:get/get.dart';
+
+import '../models/task.dart';
+
+class TaskController extends GetxController{
+
+  @override
+  void onReady(){
+    getTask();
+    super.onReady();
+  }
+
+  var taskList = <Task>[].obs;
+
+  Future<int> addTask({Task? task})async{
+    return await DBHelper.insert(task);
+  }
+
+  void getTask() async {
+    List<Map<String, dynamic>> task = await DBHelper.query();
+    taskList.assignAll(task.map((data) => new Task.fromJson(data)).toList());
+  }
+
+  void delete(Task task){
+    var val =DBHelper.delete(task);
+    getTask();
+  }
+
+  void markTaskCompleted(int id) async{
+    await DBHelper.update(id);
+    getTask();
+  }
+
+}
